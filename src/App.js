409b9a739c9779b9cacda6316443a6c86cd595ea
Brad Felix <bradfelix1@gmail.com>
2020-04-27 12:21:19 -0600

    finish up sample

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from './logo.svg';
+import './App.css';
 
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
